5e40533d8cf3e40e8b430918f67d85fac8c8eef6
Doan_na <doanna@mpos.vn>
2023-02-22 08:08:15 +0700

    Adding dist

---
+++ assets/SignOut-cda022cb.js
@@ -0,0 +1,1 @@
+import{z as e,u as o,H as a,o as c,a as n,b as u}from"./index-b601219a.js";const r={class:"w-50 m-auto p-5"},_=u("h4",{class:"text-center mb-3"},"Sign Out Page",-1),i=[_],d={__name:"SignOut",setup(h){const t=e(),{signOutAuth:s}=o();return a(async()=>{console.log("sign out"),s(),t.push("/")}),(l,p)=>(c(),n("div",r,i))}};export{d as default};
